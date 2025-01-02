e7956bcef8f6ba48b4c6aa29f6cc535f278e5c25
toxypiks <HannaHinkelbein89@gmx.de>
2025-01-02 22:07:03 +0100

    Visualize wave in raylib WIP

--- src/sine_wave.c
+++ src/sine_wave.c
@@ -20,14 +20,38 @@ int main(void) {
   SetTargetFPS(60);
 
   RayOutBuffer ray_out_buffer = create_ray_out_buffer(10000);
+
   while(!WindowShouldClose()) {
     size_t num_bytes = jack_ringbuffer_read_space(jack_stuff->ringbuffer_audio);
     if(num_bytes < 96000 * sizeof(float)) {
       gen_signal_in_buf(&osc, data_buf, 1024);
       change_time_step(&osc, 1024);
       jack_ringbuffer_write(jack_stuff->ringbuffer_audio, (void *)data_buf, 1024*sizeof(float));
+      jack_ringbuffer_write(jack_stuff->ringbuffer_video, (void *)data_buf, 1024*sizeof(float));
+
+      if(jack_stuff->ringbuffer_video){
+        float output_buffer[1024];
+        size_t num_bytes = jack_ringbuffer_read_space(jack_stuff->ringbuffer_video);
+        if(num_bytes >= (1024* sizeof(float))) {
+
+          jack_ringbuffer_read(jack_stuff->ringbuffer_video, (char*)output_buffer, 1024 * sizeof(float));
+        } else {
+          for ( int i = 0; i < 1024; i++)
+          {
+            output_buffer[i] = 0.0;
+          }
+        }
+        copy_to_ray_out_buffer(&ray_out_buffer, output_buffer, 1024);
+      }
       BeginDrawing();
       ClearBackground(BLACK);
+      for (size_t i = 0; i < ray_out_buffer.global_frames_count; ++i)
+      {
+        if (i % 100 == 0) {
+          DrawCircle(i, screen_height/2 + (int)(ray_out_buffer.global_frames[i] * 100), 5, BLUE);
+          printf("%d", (int)ray_out_buffer.global_frames[i]);
+        }
+      }
       EndDrawing();
     }
   }
