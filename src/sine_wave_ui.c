2cb58cb8b7b19c50d3f54220646cc3eba1391683
toxypiks <HannaHinkelbein89@gmx.de>
2025-01-23 15:28:37 +0100

    Show sine wave in widget

--- src/sine_wave_ui.c
+++ src/sine_wave_ui.c
@@ -197,11 +197,24 @@ typedef struct {
   float data_buf[1024];
 } SignalState;
 
-void signal_widget(Ui_Rect r, SignalState *signal_state) {
+void gen_signal_in_buf(SignalState *signal_state) {
   for (size_t i = 0; i < 1024; ++i) {
     signal_state->data_buf[i] = sinf(fmod((2*M_PI*440*i/48000.0f), 2.0*M_PI));
   }
 }
+
+void signal_widget(Ui_Rect r, SignalState *signal_state, Color c) {
+  float rw = r.w;
+  float rh = r.h;
+  float rx = r.x;
+  float ry = r.y;
+
+  for (size_t data = 0; data < 1024; ++data)
+    if (data % 10 == 0) {
+      DrawCircle(data, rh*1.5 + (int)(signal_state->data_buf[data] *100), 3, BLUE);
+    }
+}
+
 
 int main(void) {
   size_t width = 800;
@@ -218,21 +231,25 @@ int main(void) {
   SliderState slider_freq = {0};
   slider_freq.scroll = 0.0f;
 
+  SignalState signal_state = {0};
+
   while (!WindowShouldClose()) {
     float w = GetRenderWidth();
     float h = GetRenderHeight();
+
+    gen_signal_in_buf(&signal_state);
 
     BeginDrawing();
     ClearBackground(BLACK);
     layout_stack_push(&ls, LO_VERT, ui_rect(0, 0, w, h), 3, 0);
     layout_stack_push(&ls, LO_HORZ, layout_stack_slot(&ls), 2, 0);
         button_widget(layout_stack_slot(&ls), PINK);
-        widget(layout_stack_slot(&ls), PURPLE);
+        widget(layout_stack_slot(&ls), BLACK);
     layout_stack_pop(&ls);
-        widget(layout_stack_slot(&ls), BLUE);
+    signal_widget(layout_stack_slot(&ls), &signal_state, BLUE);
     layout_stack_push(&ls, LO_HORZ, layout_stack_slot(&ls), 3, 0);
         slider_widget(layout_stack_slot(&ls), &slider_vol);
-    widget(layout_stack_slot(&ls), PINK);
+        widget(layout_stack_slot(&ls), BLACK);
         slider_widget(layout_stack_slot(&ls), &slider_freq);
     layout_stack_pop(&ls);
     layout_stack_pop(&ls);
